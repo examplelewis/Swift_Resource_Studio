c12d8c264bf391cb741d47aba539f195050b1889
龚宇 <564542431@qq.com>
2022-05-14 17:23:21 +0800

    添加 OC桥接文件

---
+++ Resource Studio/Objective-C Bridging/RSBaseBridging.h
@@ -0,0 +1,12 @@
+//
+//  RSBaseBridging.h
+//  Resource Studio
+//
+//  Created by 龚宇 on 22/05/14.
+//
+
+#ifndef RSBaseBridging_h
+#define RSBaseBridging_h
+
+
+#endif /* RSBaseBridging_h */
---
+++ Resource Studio/Objective-C Bridging/RSBaseBridging.m
@@ -0,0 +1,8 @@
+//
+//  RSBaseBridging.m
+//  Resource Studio
+//
+//  Created by 龚宇 on 22/05/14.
+//
+
+#import <Foundation/Foundation.h>
