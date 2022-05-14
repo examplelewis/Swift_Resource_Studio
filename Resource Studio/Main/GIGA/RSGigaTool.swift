2e635ac9ee40b82c83083480bccc8844266cb28d
龚宇 <564542431@qq.com>
2022-05-14 23:52:39 +0800

    完善 GIGA

---
+++ Resource Studio/Main/GIGA/RSGigaTool.swift
@@ -0,0 +1,19 @@
+//
+//  RSGigaTool.swift
+//  Resource Studio
+//
+//  Created by 龚宇 on 22/05/14.
+//
+
+import Foundation
+
+func RSGigaWorkImageNameFrom(url: String, oldFileName: String) -> String {
+    let urlComponets = url.components(separatedBy: "/")
+    let series = urlComponets[urlComponets.count - 3]
+    let newSeries = series.uppercased().appending("-")
+    let workID = urlComponets[urlComponets.count - 2]
+    var newName = workID.replacingOccurrences(of: series, with: newSeries)
+    newName = newName.appendingFormat(".%@", (oldFileName as NSString).pathExtension)
+    
+    return newName
+}
