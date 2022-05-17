e677b1ea89f2f8b090d33fdae1c8150a2274798b
龚宇 <564542431@qq.com>
2022-05-17 23:14:12 +0800

    完善 Giga

--- Resource Studio/Main/GIGA/RSGigaTagInfoFetcher.swift
+++ Resource Studio/Main/GIGA/RSGigaTagInfoFetcher.swift
@@ -11,7 +11,12 @@ import hpple
 protocol RSGigaTagInfoFetcherDelegate: AnyObject {
     func gigaTagInfoFetcherDidFinish()
 }
- 
+
+// 使用方法
+// var fetcher: RSGigaTagInfoFetcher?
+// fetcher = RSGigaTagInfoFetcher(tagIDs: [366, 486, 232, 31616, 30013, 494, 487, 31720, 35, 353, 459, 31717, 92, 212, 30553, 31597, 32027, 37, 227, 30629, 31834, 481, 31694, 30219, 545, 96, 30829, 363, 31631, 31624, 31710, 30023, 31628, 412, 30600, 201, 300, 519, 30710, 31691, 30495, 30300, 31695, 73, 31783])
+// fetcher?.start(isFirstTag: true)
+
 class RSGigaTagInfoFetcher {
     weak var delegate: RSGigaTagInfoFetcherDelegate?
     
--- Resource Studio/ViewController.swift
+++ Resource Studio/ViewController.swift
@@ -9,7 +9,6 @@ import Cocoa
 import Alamofire
 
 class ViewController: NSViewController, RSGigaTagInfoFetcherDelegate {
-    var fetcher: RSGigaTagInfoFetcher?
     
     // MARK: Lifecycle
     override func viewDidLoad() {
@@ -19,8 +18,7 @@ class ViewController: NSViewController, RSGigaTagInfoFetcherDelegate {
     }
     
     @IBAction func tempButtonDidPress(_ sender: NSButton) {
-//        fetcher = RSGigaTagInfoFetcher(tagIDs: [366, 486, 232, 31616, 30013, 494, 487, 31720, 35, 353, 459, 31717, 92, 212, 30553, 31597, 32027, 37, 227, 30629, 31834, 481, 31694, 30219, 545, 96, 30829, 363, 31631, 31624, 31710, 30023, 31628, 412, 30600, 201, 300, 519, 30710, 31691, 30495, 30300, 31695, 73, 31783])
-//        fetcher?.start(isFirstTag: true)
+        
     }
     
     // MARK: RSGigaTagInfoFetcherDelegate
