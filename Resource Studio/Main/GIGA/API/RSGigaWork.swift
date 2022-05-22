70027708aabb86e109fce287b55c43b2f8aad141
龚宇 <564542431@qq.com>
2022-05-22 15:08:43 +0800

    完善 Giga

---
+++ Resource Studio/Main/GIGA/API/RSGigaWork.swift
@@ -0,0 +1,16 @@
+//
+//  RSGigaWork.swift
+//  Resource Studio
+//
+//  Created by 龚宇 on 22/05/22.
+//
+
+import Cocoa
+
+class RSGigaWork: GYHTTPBaseModel {
+    var tagName = ""
+    var workName = ""
+    var workURL = ""
+    var workImageURL = ""
+    var time = ""
+}
