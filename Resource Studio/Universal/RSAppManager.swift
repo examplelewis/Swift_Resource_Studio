06aa52237565e7b5ee6284c0208f455b1da6565b
龚宇 <564542431@qq.com>
2022-06-07 23:01:41 +0800

    完善 App 根目录

--- Resource Studio/Universal/RSAppManager.swift
+++ Resource Studio/Universal/RSAppManager.swift
@@ -15,7 +15,7 @@ class RSAppManager {
     
     // MARK: Setup
     func setup() {
-        GYBase.shared.mainFolderPath = "/Users/Mercury/SynologyDrive/同步文档/Resource Studio"
+        GYBase.shared.mainFolderPath = "/Users/Mercury/SynologyDrive/同步文档/App Data/Resource Studio"
         
         GYLogManager.shared.update(defaultColor: NSColor.labelColor, successColor: NSColor.systemGreen, warningColor: NSColor.systemYellow, errorColor: NSColor.systemRed, font: NSFont(name: "PingFangSC-Regular", size: 12.0)!)
     }
