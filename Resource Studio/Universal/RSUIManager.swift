8e82abdd3d372e163a7f6f037b314d11825f1d46
龚宇 <564542431@qq.com>
2022-08-01 19:57:18 +0800

    1、完善 ViewController 的配置

--- Resource Studio/Universal/RSUIManager.swift
+++ Resource Studio/Universal/RSUIManager.swift
@@ -18,12 +18,27 @@ class RSUIManager {
     
     // MARK: Progress Indicator
     func resetProgressIndicator() {
+        progressIndicatorMaxValue = 1.0
         
+        GYAsyncSafe {
+            self.viewController.progressIndicator.doubleValue = 0.0
+            self.viewController.progressIndicator.maxValue = 1.0
+            self.viewController.progressLabel.stringValue = "0 / 0"
+        }
     }
     func updateProgressIndicatorMaxValue(_ value: Double) {
+        progressIndicatorMaxValue = value
         
+        GYAsyncSafe {
+            self.viewController.progressIndicator.doubleValue = 0.0
+            self.viewController.progressIndicator.maxValue = value
+            self.viewController.progressLabel.stringValue = String(format: "0 / %.0f", value)
+        }
     }
     func updateProgressIndicatorValue(_ value: Double) {
-        
+        GYAsyncSafe {
+            self.viewController.progressIndicator.doubleValue = value
+            self.viewController.progressLabel.stringValue = String(format: "%.0f / %.0f", value, self.progressIndicatorMaxValue)
+        }
     }
 }
