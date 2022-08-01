9f4172c9fbcb87284d943a4d39894c0fc00d4955
龚宇 <564542431@qq.com>
2022-08-01 19:55:41 +0800

    1、完善 DownloadMenuItem

--- Resource Studio/AppDelegate.swift
+++ Resource Studio/AppDelegate.swift
@@ -77,7 +77,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         GYLogManager.shared.update(defaultColor: NSColor.labelColor, successColor: NSColor.systemGreen, warningColor: NSColor.systemYellow, errorColor: NSColor.systemRed, font: NSFont(name: "PingFangSC-Regular", size: 12.0)!)
     }
     func setupDownloads() {
-        GYDownloadSettings.shared.updateMenuItems(withBaseTag: 12000000, callback: { [weak self] (menu: NSMenu) in
+        GYDownloadSettings.shared.updateMenuItems(withBaseTag: RSDownloadMenuItemBaseTag, callback: { [weak self] (menu: NSMenu) in
             self?.downloadRootMenuItem.submenu = menu
         })
     }
---
+++ Resource Studio/Universal/RSConsts.swift
@@ -0,0 +1,14 @@
+//
+//  RSConsts.swift
+//  Resource Studio
+//
+//  Created by jsyh on 2022/8/1.
+//
+
+import Foundation
+
+// MARK: Tags
+let RSDownloadMenuItemBaseTag = 13000000
+
+// MARK: PATHs
+let RSPixivUtilDBFilePath = "/SynologyDrive/Tool/pixivutil/db.sqlite"
